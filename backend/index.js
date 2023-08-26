ac5dda90dd19736d0e367699a536ecf66ce36af0
kislay1 <kislaynitr69@gmail.com>
2023-08-26 14:35:06 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const mongoDB=require("./db")
 mongoDB();
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "https://sprightly-stardust-09f88e.netlify.app/");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
