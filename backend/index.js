94005730cddfd5a543446a81ad5966df00c14595
kislay1 <kislaynitr69@gmail.com>
2023-08-27 19:40:36 +0530

    render1

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ mongoDB();
 
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://cravehub.netlify.app/');
+  res.setHeader('Access-Control-Allow-Origin', 'https://cravehub.netlify.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
